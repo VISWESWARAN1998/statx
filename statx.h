6d8fcbf533da7ccd040b67ff7420f52247de4bad
VISWESWARAN1998 <visweswaran.nagasivam98@gmail.com>
2018-07-08 11:46:40 +0530

    Update statx.h

--- statx.h
+++ statx.h
@@ -13,32 +13,27 @@ SOME GOLDEN RULES:
 ------------------
 
 1.	Don't let the goat(variable) to stay longer in your garden(cpu) it will eat
-	all the grasses(memory). => Keep the persistance of variable as low as possible.
+all the grasses(memory). => Keep the persistance of variable as low as possible.
 
 2.	You don't give strange names to your kids so, don't give strange names to
-	your goats(variables). => Naming of variable should be done with care.
+your goats(variables). => Naming of variable should be done with care.
 
 3.	20% of the code will consume 80% of time, optimzing the 20% of the code
-	will reduce 80% running time. => The Pareto principle
+will reduce 80% running time. => The Pareto principle
 
 4.	20% of the code will do 80% of the work, optimizing the 20% of the code
-	will reduce 80% of running time. => The Pareto principle
+will reduce 80% of running time. => The Pareto principle
 
-5.  Math is about accuracy not robustness! and robustness is inversely 
-	proportional to accuracy. You are needed to write a code which
-	gives accurate results for computation and that speaking expect exceptions
-	for faulty input and note that it is YOUR input which is faulty!
+5.  Math is about accuracy not robustness! and robustness is inversely
+proportional to accuracy. You are needed to write a code which
+gives accurate results for computation and that speaking expect exceptions
+for faulty input and note that it is YOUR input which is faulty!
 
 6.  Don't waste your time on writing clean code and don't save your time on writing
-	quick and drity code. Write maintainable code and ship it on time, make it
-	gorgeous later.
+quick and drity code. Write maintainable code and ship it on time, make it
+gorgeous later.
 
 */
-
-
-
-
-
 
 
 //===================================================================
@@ -52,11 +47,11 @@ public:
 	T get_mean(std::vector<T> vec)
 	{
 		T total = 0;
-		for(auto i : vec) total+=i;
-		auto average = total/vec.size();
+		for (auto i : vec) total += i;
+		auto average = total / vec.size();
 		return average;
 	}
-		
+
 };
 
 
@@ -67,16 +62,16 @@ template<typename T>
 class median
 {
 public:
-	T get_median(std::vector<T> vec, bool sorted=false)
+	T get_median(std::vector<T> vec, bool sorted = false)
 	{
-		if(sorted==false)std::sort(vec.begin(), vec.end());
+		if (sorted == false)std::sort(vec.begin(), vec.end());
 		auto vector_size = vec.size();
-		if(vector_size == 1) return vec[0];
+		if (vector_size == 1) return vec[0];
 		// If the elements are odd return the middle element
-		if(vector_size%2 != 0) return vec[vector_size/2];
+		if (vector_size % 2 != 0) return vec[vector_size / 2];
 		// If the elements count are even return the average of middle elements
-		auto middle_element_one = vector_size/2;
-		auto middle_element_two = middle_element_one -1;
+		auto middle_element_one = vector_size / 2;
+		auto middle_element_two = middle_element_one - 1;
 		auto result = (vec[middle_element_one] + vec[middle_element_two]) / 2;
 		return result;
 
@@ -98,24 +93,24 @@ public:
 		std::map<T, unsigned long int> number_count_table;
 		std::vector<T> elements_with_max_occurences;
 		unsigned long int bigger_no = 1;
-		for(auto i : vec)
+		for (auto i : vec)
 		{
-			if(number_count_table.find(i) == number_count_table.end()) number_count_table[i] = 1;
-			else{
+			if (number_count_table.find(i) == number_count_table.end()) number_count_table[i] = 1;
+			else {
 				auto current_count = number_count_table[i];
 				current_count++;
-				if(current_count>bigger_no) bigger_no = current_count;
+				if (current_count>bigger_no) bigger_no = current_count;
 				number_count_table[i] = current_count;
 			}
 		}
-		if(bigger_no==1) {
+		if (bigger_no == 1) {
 			return vec[0];
 		}
 		else
 		{
-			for(auto itr : number_count_table)
+			for (auto itr : number_count_table)
 			{
-				if(itr.second == bigger_no) elements_with_max_occurences.push_back(itr.first);
+				if (itr.second == bigger_no) elements_with_max_occurences.push_back(itr.first);
 			}
 			std::sort(elements_with_max_occurences.begin(), elements_with_max_occurences.end());
 			return elements_with_max_occurences[0];
@@ -140,15 +135,15 @@ public:
 		this->vector_size = vec.size();
 		T numerator = 0;
 		T total_weight = 0;
-		for(unsigned long int i=0; i<vector_size; i++)
+		for (unsigned long int i = 0; i<vector_size; i++)
 		{
 			T current_value = vec[i] * weights[i];
-			numerator+=current_value;
+			numerator += current_value;
 			total_weight += weights[i];
 		}
 		//std::cout << "NUMERATOR: " << numerator << "\n";
 		//std::cout << "DENOMINATOR: " << summation_of_weights << "\n";
-		return numerator/total_weight;
+		return numerator / total_weight;
 	}
 };
 
@@ -161,19 +156,19 @@ class standard_deviation : public mean<T>
 {
 private:
 	T mean_value;
-	T standard_deviation;
+	T standard_deviation_value;
 public:
 	T get_standard_deviation(std::vector<T> vec)
 	{
 		this->mean_value = this->get_mean(vec);
-		this->standard_deviation = 0;
-		for(unsigned long int i = 0; i<vec.size(); ++i)
+		this->standard_deviation_value = 0;
+		for (unsigned long int i = 0; i<vec.size(); ++i)
 		{
-			T powered_value = (vec[i]-this->mean_value) * (vec[i]-this->mean_value);
-			this->standard_deviation+=powered_value;
+			T powered_value = (vec[i] - this->mean_value) * (vec[i] - this->mean_value);
+			this->standard_deviation_value += powered_value;
 		}
-		this->standard_deviation = sqrt(this->standard_deviation/vec.size());
-		return this->standard_deviation;
+		this->standard_deviation_value = sqrt(this->standard_deviation_value / vec.size());
+		return this->standard_deviation_value;
 	}
 };
 
@@ -183,41 +178,41 @@ public:
 //==========================================================================================
 /*
 
-			CALCULATING INTERQUARTILE RANGE FOR ODD NO OF ELEMENTS:
-			-------------------------------------------------------
+CALCULATING INTERQUARTILE RANGE FOR ODD NO OF ELEMENTS:
+-------------------------------------------------------
 
-			Given Elements(E) = { 3,5,5,7,8,8,9,10,13,13,14,15,16,22,23}
+Given Elements(E) = { 3,5,5,7,8,8,9,10,13,13,14,15,16,22,23}
 
-			Step 1: Sort it! Here we already have an sorted values
+Step 1: Sort it! Here we already have an sorted values
 
-			E = { 3, 5, 5, 7, 8, 8, 9, 10, 13, 13, 14, 15, 16, 22, 23 }
+E = { 3, 5, 5, 7, 8, 8, 9, 10, 13, 13, 14, 15, 16, 22, 23 }
 
-			Step 2: Get the median for the values
+Step 2: Get the median for the values
 
-			Median (M) = 10
+Median (M) = 10
 
-			Step 3: The lower and the upper half
+Step 3: The lower and the upper half
 
-			Elements which are left to the median are called left half and
-			to the right is right half
+Elements which are left to the median are called left half and
+to the right is right half
 
-				3 5 5 7 8 8 9   10    13 13 14 15 16 22 23
-				|___________|    |    |__________________|
-				LOWER HALF     MEDIAN   UPPER HALF
+3 5 5 7 8 8 9   10    13 13 14 15 16 22 23
+|___________|    |    |__________________|
+LOWER HALF     MEDIAN   UPPER HALF
 
-			Step 4:
-				Q1 = median of left half
-				Q3 = median of right half
+Step 4:
+Q1 = median of left half
+Q3 = median of right half
 
-				interquartile_range = Q3 - Q1
-			
-			CALCULATING INTERQUARTILE RANGE FOR EVEN NO OF ELEMENTS:
-			--------------------------------------------------------
-			Step 1: Sort the array
-			Step 2: Get the median of the array
-			Step 3: Insert the median back to the array
-			Step 4: Follow odd no procedure since we have an array
-				of odd size.
+interquartile_range = Q3 - Q1
+
+CALCULATING INTERQUARTILE RANGE FOR EVEN NO OF ELEMENTS:
+--------------------------------------------------------
+Step 1: Sort the array
+Step 2: Get the median of the array
+Step 3: Insert the median back to the array
+Step 4: Follow odd no procedure since we have an array
+of odd size.
 
 */
 template <typename T>
@@ -226,12 +221,12 @@ class interquartile_range : public median<T>
 private:
 	bool is_odd_vector;
 public:
-	T get_interquartile_range(std::vector<T> vec, bool sorted=false)
+	T get_interquartile_range(std::vector<T> vec, bool sorted = false)
 	{
-		if(sorted==false) std::sort(vec.begin(), vec.end());
-		if(vec.size() %2 != 0) is_odd_vector = true;
+		if (sorted == false) std::sort(vec.begin(), vec.end());
+		if (vec.size() % 2 != 0) is_odd_vector = true;
 		else is_odd_vector = false;
-		if(is_odd_vector)
+		if (is_odd_vector)
 		{
 			return compute(vec);
 		}
@@ -239,7 +234,7 @@ public:
 		{
 			unsigned long int middle_index = vec.size() / 2;
 			T median_for_vector = this->get_median(vec);
-			vec.insert(vec.begin()+middle_index, median_for_vector);
+			vec.insert(vec.begin() + middle_index, median_for_vector);
 			return compute(vec);
 		}
 	}
@@ -248,19 +243,19 @@ private:
 	{
 		unsigned long int middle_element_index = vec.size() / 2;
 		std::vector<T> lower_half;
-		for(unsigned long int i = 0; i < middle_element_index; i++)
+		for (unsigned long int i = 0; i < middle_element_index; i++)
 		{
 			lower_half.push_back(vec[i]);
 		}
 		std::vector<T> upper_half;
-		for(unsigned long int i = middle_element_index+1; i<vec.size(); i++)
+		for (unsigned long int i = middle_element_index + 1; i<vec.size(); i++)
 		{
 			upper_half.push_back(vec[i]);
 		}
 
 		T q1 = this->get_median(lower_half);
 		T q3 = this->get_median(upper_half);
-		return q3-q1;
+		return q3 - q1;
 	}
 };
 
@@ -275,9 +270,9 @@ class frequency_map_converter
 public:
 	void to_vector(std::map<T, unsigned long int> frequency_map, std::vector<T> &target_vector)
 	{
-		for(auto element : frequency_map)
+		for (auto element : frequency_map)
 		{
-			for(unsigned long int i = 0; i < element.second; i++) target_vector.push_back(element.first);
+			for (unsigned long int i = 0; i < element.second; i++) target_vector.push_back(element.first);
 		}
 	}
 };
@@ -290,26 +285,26 @@ public:
 
 /*
 
-					HOW TO CALCULATE THE RANGE
-					--------------------------
+HOW TO CALCULATE THE RANGE
+--------------------------
 
-					sorted input vector = { 1, 2, 3, 4}
-					greatest_value = 4
-					least_value = 1
-					range = greatest_value - least_value
-					i.e range = 3
+sorted input vector = { 1, 2, 3, 4}
+greatest_value = 4
+least_value = 1
+range = greatest_value - least_value
+i.e range = 3
 */
 
 template <typename T>
 class range
 {
 public:
-	T get_range(std::vector<T> vec, bool sorted=false)
+	T get_range(std::vector<T> vec, bool sorted = false)
 	{
-		if(sorted==false) std::sort(vec.begin(), vec.end());
+		if (sorted == false) std::sort(vec.begin(), vec.end());
 		T greatest_value = vec[vec.size() - 1];
 		T least_value = vec[0];
-		return greatest_value-least_value;
+		return greatest_value - least_value;
 	}
 };
 
@@ -323,29 +318,69 @@ template <typename T>
 class quartile : public median<T>
 {
 public:
-	std::map<std::string, T> get_quartile(std::vector<T> vec, bool sorted=false)
+	std::map<std::string, T> get_quartile(std::vector<T> vec, bool sorted = false)
 	{
-		if(sorted==false) std::sort(vec.begin(), vec.end());
+		if (sorted == false) std::sort(vec.begin(), vec.end());
 		std::map<std::string, T> result;
-		result["q2"] = this->get_median(vec, sorted=true);
+		result["q2"] = this->get_median(vec, sorted = true);
 		unsigned long int middle_index = vec.size() / 2;
 		std::vector<T> lower_half;
-		for(unsigned long int i=0; i<middle_index; i++)
+		for (unsigned long int i = 0; i<middle_index; i++)
 		{
 			lower_half.push_back(vec[i]);
 		}
-		result["q1"] = this->get_median(lower_half, sorted=true);
+		result["q1"] = this->get_median(lower_half, sorted = true);
 		// free the memory by clearning the lower half
 		lower_half.clear();
 		std::vector<T> upper_half;
-		if(vec.size()%2!=0) middle_index++;
-		for(unsigned long int i = middle_index; i<vec.size(); i++)
+		if (vec.size() % 2 != 0) middle_index++;
+		for (unsigned long int i = middle_index; i<vec.size(); i++)
 		{
 			upper_half.push_back(vec[i]);
 		}
-		result["q3"] = this->get_median(upper_half, sorted=true);		
+		result["q3"] = this->get_median(upper_half, sorted = true);
 		return result;
 	}
 };
 
+
+// ====================== TEXT TO NUMERICAL DATA CONVERTERS ===================
+
+template <typename T>
+class LabelEncoder
+{
+private:
+	long int current_encoded_value = -1;
+	std::vector<T> array;
+	std::map < T, long int> encoded_values;
+public:
+	void fit(std::vector<T> array)
+	{
+		this->array = array;
+		std::sort(array.begin(), array.end());
+		std::vector<T> sorted_array = array;
+		for (auto i : sorted_array)
+		{
+			if (encoded_values.find(i) == encoded_values.end()) {
+				current_encoded_value++;
+				encoded_values[i] = current_encoded_value;
+			}
+		}
+	}
+
+	std::vector<long int> transform(std::vector<T> array)
+	{
+		std::vector<long int> transformed_array;
+		for (auto i : array)
+		{
+			transformed_array.push_back(encoded_values[i]);
+		}
+		return transformed_array;
+	}
+
+	std::vector<long int> transform()
+	{
+		return transform(this->array);
+	}
+};
 
